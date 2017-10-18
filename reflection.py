eff4bb1b8470a3b7eaec0bc7d3b5f4aaa0e13a16
lbrl <00000001h@gmail.com>
2017-10-19 01:21:44 +0900

    Update reflection.py

--- reflection.py
+++ reflection.py
@@ -32,16 +32,6 @@ def line_intersection(line1, line2):
     y = det(d, ydiff) / div
     return [x, y]
 
-def propogate1(x, p):
-    B = [0., 0.]
-    B[0] = 1.e3 + x[0]
-    k = p[1] / p[0]
-    B[1] = x[1] + k*(B[0]-x[0])
-    C = [40., 40.]
-    D = [120., -40.]
-    IP = line_intersection((x, B), (C, D))
-    return [IP[0], IP[1]]
-
 def reflect(p, isPrint=False):
     # beta = 5.*math.pi/4.
     beta = math.pi/4.
@@ -65,20 +55,6 @@ def reflect(p, isPrint=False):
         print 'kx = {},  ky = {},  |k| = {}'.format(kx, ky, (kx**2+ky**2)**.5)
         print '*'*40
     return [kx, ky]
-
-def propogate2(x, p):
-    # A = [0., 0.]
-    # A[0] = -1.e3 + x[0]
-    # k = p[1] / p[0]
-    # A[1] = x[1] + k*(A[0]-x[0])
-    B = [0., 0.]
-    B[0] = 1.e3 + x[0]
-    k = p[1] / p[0]
-    B[1] = x[1] + k*(B[0]-x[0])
-    C = [60., -120.]
-    D = [180., -120.]
-    IP = line_intersection((x, B), (C, D))
-    return [IP[0], IP[1]]
 
 def propogate(x, p, C, D):
     B = [x[0]+1., 0.]
@@ -111,7 +87,13 @@ def propogate11(x, p):
 
 def main():
     isDrawRays = True
-    c1 = r.TCanvas('c1', 'c1', 800, 800)
+    isDrawRays = False
+    D = 48.# Lens' diameter.
+    csi = [50., -60., 50., 60.]
+    dx = 60*2**-.5
+    mirror = [120-dx, dx, 120+dx, -dx]
+    lens = [120-D/2, -120., 120+D/2, -120.]
+    c1 = r.TCanvas('c1', 'c1', 600, 600)
     c1.DrawFrame(-50, -150, 250, 150)
     c1.SetGrid()
     # frame = r.TFrame(-1e3, -1e3, 1e3, 1e3)
@@ -123,21 +105,19 @@ def main():
     line.SetLineColor(r.kBlack)
     line.SetLineStyle(1)
     line.SetLineWidth(3)
-    line.DrawLine(0, -60, 0, 60)
-    dx = 60*2**-.5
-    line.DrawLine(120-dx, dx, 120+dx, -dx)
-    line.DrawLine(60, -120, 180, -120)
-    for i in xrange(100):
+    line.DrawLine(csi[0], csi[1], csi[2], csi[3])
+    line.DrawLine(mirror[0], mirror[1], mirror[2], mirror[3])
+    line.DrawLine(lens[0], lens[1], lens[2], lens[3])
+    for i in xrange(100000):
         x = get_point()
-        # print 'x', x
         p = get_direction()
-        # print 'p', p
         y = propogate11(x, p)
-        # print 'y', y
+        if y[1] < min(mirror[1], mirror[3]):
+            continue
+        if y[1] > max(mirror[1], mirror[3]):
+            continue
         k = reflect(p)
-        # print 'k', k
         z = propogate22(y, k)
-        # print 'z', z
         #############
         hx.Fill(x[1])
         hy_x.Fill(y[0]-120)
@@ -159,12 +139,13 @@ def main():
             line.SetLineWidth(4)
             line.DrawLine(y[0], y[1], y[0]+10., y[1]+k[1]/k[0]*10.)
     c1.Update()
-    c2 = r.TCanvas('c2', 'c2', 800, 800)
+    c2 = r.TCanvas('c2', 'c2', 600, 600)
     hx.SetLineColor(r.kRed)
     hz.SetLineColor(r.kBlue)
     hx.Draw()
     hy_x.Draw('same')
     hz.Draw('same')
+    print 'The number of photons reached the lens is %d.' % hz.Integral(hz.FindBin(-D/2), hz.FindBin(D/2))
     c2.Update()
     raw_input()
 
